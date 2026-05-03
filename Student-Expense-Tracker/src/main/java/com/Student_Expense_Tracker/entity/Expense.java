bd1ef9b2ed3e42b1e9aa8380e08cbcd6fc5d99fa
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-05-03 21:21:36 +0530

    create Expense entity

---
+++ Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/entity/Expense.java
@@ -0,0 +1,38 @@
+package com.Student_Expense_Tracker.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private  String cateogry;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column
+    private String description;
+
+}
