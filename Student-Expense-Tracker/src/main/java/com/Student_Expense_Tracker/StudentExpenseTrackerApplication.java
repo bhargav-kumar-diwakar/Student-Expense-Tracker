7d5bc2c7122546bcbacc6b7d87899c8a5268d062
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-05-03 21:00:46 +0530

    Initial commit

---
+++ Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/StudentExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Student_Expense_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentExpenseTrackerApplication.class, args);
+	}
+
+}
