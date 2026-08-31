454d6553e82ab7c0759113523d8f728b8495fe14
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-08-31 13:37:47 +0530

    Add LoginDTO file

---
+++ Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/dto/LoginDTO.java
@@ -0,0 +1,13 @@
+package com.Student_Expense_Tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private String email;
+    private String password;
+}
