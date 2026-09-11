860e6e8cb792e130fd3b454bfcac7e35112bdf40
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-09-11 19:52:21 +0530

    add custom repository methods to query expenses by user,category and date range

--- Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/repository/ExpenseRepository.java
+++ Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/repository/ExpenseRepository.java
@@ -2,10 +2,12 @@ package com.Student_Expense_Tracker.repository;
 
 import com.Student_Expense_Tracker.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
@@ -18,4 +20,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     //fetch expenses greater than a certain amount
     List<Expense> findByAmountGreaterThan(Double amount);
+
+    //Find expense by id AND user - prevents user A accessing user B's expense
+    Optional<Expense> findByIdandUser(Long id, User user);
 }
