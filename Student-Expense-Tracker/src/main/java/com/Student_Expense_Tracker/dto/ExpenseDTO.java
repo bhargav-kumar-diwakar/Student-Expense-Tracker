daec2a50df768208a1cf14cf0d347b46fd5896ef
Bhargav Kumar Diwakar <bhargavkumardiwakar@gmail.com>
2026-09-16 11:47:26 +0530

    Fix the errors

--- Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/dto/ExpenseDTO.java
+++ Student-Expense-Tracker/src/main/java/com/Student_Expense_Tracker/dto/ExpenseDTO.java
@@ -1,8 +1,11 @@
 package com.Student_Expense_Tracker.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
@@ -12,9 +15,18 @@ import java.time.LocalDate;
 public class ExpenseDTO {
 
     private Long id;
+
+    @NotBlank(message = "Title cannot be empty")
     private String title;
+
+    @NotNull(message = "Amount cannot be null")
+    @Positive(message = "Amount must be greate than zero")
     private Double amount;
+
+    @NotBlank(message = "Category cannot be empty")
     private String category;
+
+    @NotNull(message = "Date cannot be null")
     private LocalDate date;
     private String description;
 
